b8c3522ad3e317dd910ba84b521c8ce35776793a
grintender <d-mon3@yandex.ru>
2016-06-06 17:25:06 +0400

    yet another commit about spaces
    yet another commit about spaces

--- build/js/game.js
+++ build/js/game.js
@@ -260,14 +260,14 @@
 
   CanvasRenderingContext2D.prototype.wrapText = function(text, x, y, maxWidth, lineHeight) {
 
-  var lines = text.split('\n');
+    var lines = text.split('\n');
 
-  for (var i = 0; i < lines.length; i++) {
+    for (var i = 0; i < lines.length; i++) {
 
-      var words = lines[i].split(' ');
-      var line = '';
+    var words = lines[i].split(' ');
+    var line = '';
 
-      for (var n = 0; n < words.length; n++) {
+    for (var n = 0; n < words.length; n++) {
         var testLine = line + words[n] + ' ';
         var metrics = this.measureText(testLine);
         var testWidth = metrics.width;
@@ -280,8 +280,8 @@
         }
       }
 
-      this.fillText(line, x, y);
-       y += lineHeight;
+    this.fillText(line, x, y);
+    y += lineHeight;
     }
   };
 
