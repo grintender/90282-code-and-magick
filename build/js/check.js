477b676c48d059dad85e658178a4d3490b126094
grintender <d-mon3@yandex.ru>
2016-06-06 10:39:37 +0400

    working module1-task2
    got it

--- build/js/check.js
+++ build/js/check.js
@@ -6,28 +6,31 @@ function getMessage(a, b){
 //var result;
 
 if (typeof a === "boolean") {
-  if (a) {
-    result = "Я попал в " + b;
-} else {
-  result = "Я никуда не попал";
-}
+      if (a) {
+        result = "Я попал в " + b;
+        return result;
+      } else {
+        result = "Я никуда не попал";
+        return result;
+      }
+
 } else if (typeof a === "number") {
-  result = "Я прыгнул на " + a * 100 + " сантиметров";
-} else if (typeof a === "array" && typeof b === "array") {
+        result = "Я прыгнул на " + (a * 100) + " сантиметров";
+        return result;
 
-  var length= 0;
-for(i=0; i < a.length; i++) {
-    length += a[i]*b[i];
-}
-  result = "Я прошёл " + length + " метров";
-} else if (typeof a === "array") {
-  var sum;
-  for(i=0; i < a.length; i++) {
-      sum += a[i];
-  }
-  result = "Я прошёл " + sum + " шагов";
-}
+} else if (Array.isArray(a) && Array.isArray(b)) {
+        var length= 0;
+        a.forEach(function(item, i){
+        length = item * b[i];
+        });
+        result = "Я прошёл " + length + " метров";
+        return result;
 
-  return result;
+} else if (Array.isArray(a)) {
+        var sum = a.reduce(function(previousValue, currentItem) {
+                return previousValue + currentItem;
+        });
+                result = "Я прошёл " + sum + " шагов";
+                return result;
+        }
 };
-getMessage();
